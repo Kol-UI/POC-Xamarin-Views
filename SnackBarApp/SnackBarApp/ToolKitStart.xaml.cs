0cd6d395c3e9e432266d4b049cced9dbabcaa125
Kol <62248045+Kol-UI@users.noreply.github.com>
2022-09-26 14:15:35 +0200

    Add files via upload

---
+++ SnackBarApp/SnackBarApp/ToolKitStart.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.CommunityToolkit;
+using Xamarin.Forms;
+
+namespace SnackBarApp
+{
+    public partial class ToolKitStart : ContentPage
+    {
+        public ToolKitStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
