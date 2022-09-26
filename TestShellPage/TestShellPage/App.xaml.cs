f2a3c59623d6108b80e04c34b0485949bd9357a8
Kol <62248045+Kol-UI@users.noreply.github.com>
2022-09-26 14:02:42 +0200

    Add files via upload

---
+++ TestShellPage/TestShellPage/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TabbedPages.Services;
+using TabbedPages.Views;
+
+[assembly: ExportFont("MaterialIconsRegular.ttf", Alias = "Material")]
+namespace TabbedPages
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TestShellPage/TestShellPage/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPages.Views
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
