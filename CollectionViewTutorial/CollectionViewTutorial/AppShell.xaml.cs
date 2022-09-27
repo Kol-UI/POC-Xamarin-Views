46a9939357f44671bd4e4d2dffe38fae05fd165d
Kol <62248045+Kol-UI@users.noreply.github.com>
2022-09-27 14:04:21 +0200

    Add files via upload

---
+++ CollectionViewTutorial/CollectionViewTutorial/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using CollectionViewTutorial.ViewModels;
+using CollectionViewTutorial.Views;
+using Xamarin.Forms;
+
+namespace CollectionViewTutorial
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ CollectionViewTutorial/CollectionViewTutorial/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using CollectionViewTutorial.Models;
+using CollectionViewTutorial.ViewModels;
+
+namespace CollectionViewTutorial.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
